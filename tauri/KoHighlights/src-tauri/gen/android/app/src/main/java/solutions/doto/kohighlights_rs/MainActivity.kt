d74357b307a6d857bbdffe1d2ddc6d95e493e2b1
Nik Rusakov <nik@doto.solutions>
2026-04-15 15:43:04 +1000

    chore(android): init tauri android setup

---
+++ tauri/KoHighlights/src-tauri/gen/android/app/src/main/java/solutions/doto/kohighlights_rs/MainActivity.kt
@@ -0,0 +1,11 @@
+package solutions.doto.kohighlights_rs
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
